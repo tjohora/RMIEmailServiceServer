86dba5712f63ddb1c50d06f3e9c901a1b288d907
TJ <tjohora@gmail.com>
2021-03-28 11:29:38 +0100

    Got server working

--- src/emailservice/EmailServer.java
+++ src/emailservice/EmailServer.java
@@ -5,10 +5,52 @@
  */
 package emailservice;
 
+import ObjectStore.EmailStore;
+import ObjectStore.UserStore;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
 /**
  *
  * @author TJ
  */
 public class EmailServer {
-    
+    public static void main(String[] args){
+        final int MY_PORT = 16000;
+        boolean continueRunning = true;
+        
+        EmailStore emailstore = new EmailStore();
+        UserStore userstore = new UserStore();
+        
+        try{
+            ServerSocket connectionSocket = new ServerSocket(MY_PORT);
+            ThreadGroup group = new ThreadGroup("Client threads");
+            group.setMaxPriority(Thread.currentThread().getPriority() - 1);
+            
+            
+            System.out.println("Now ready to accept requests.");
+            
+            while (continueRunning){
+                Socket clientLink = connectionSocket.accept();
+                //EmailService newClient = new EmailService(clientLink, emailstore, userstore);
+                //EmailService newClient = new EmailService(group, clientLink.getInetAddress()+"", clientLink, emailstore, userstore);
+                EmailService newClient = new EmailService(clientLink, emailstore, userstore);
+                //newClient.start();
+                Thread clientWrapper = new Thread(newClient);
+                clientWrapper.start();
+            }
+            
+            connectionSocket.close();
+        }catch (SocketException ex)
+        {
+            System.out.println("A problem occurred when creating the socket on port " + MY_PORT);
+            System.out.println(ex.getMessage());
+        } catch (IOException ex)
+        {
+            System.out.println("A problem occurred when reading in a message from the stream.");
+            System.out.println(ex.getMessage());
+        }
+    }
 }
--- src/emailservice/EmailService.java
+++ src/emailservice/EmailService.java
@@ -10,41 +10,148 @@ import Commands.LoginCommand;
 import Commands.RegisterCommand;
 import ObjectStore.EmailStore;
 import ObjectStore.UserStore;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author TJ
  */
-public class EmailService {
+public class EmailService implements Runnable {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Command command = new LoginCommand();
-        System.out.println(command.execute());
-        
-        UserStore users = new UserStore();
-        users.login("Test", "123456");
-        //users.test();
-        
-        
-        String test = "LOGIN%%";
-        
-        EmailStore emails = new EmailStore();
-        emails.sendEmail("Bob", "1/11/1111", "FirstEmail", "Hi, this is an email", "Jon");
-        emails.sendEmail("Bob", "1/11/1111", "SecEmail", "qwetretrt", "Jon");
-
-        emails.sendEmail("Jon", "1/11/1111", "123Email", "TestLorem", "Bob");
-        //emails.test();
-        
-        //System.out.println(emails.getAllUnreadEmails("Jon").toString());
-        
-        System.out.println(emails.getAllUnreadEmails("Jon").toString());
-        System.out.println(emails.getAllUnreadEmails("Jon").toString());
-        //emails.getAllUnreadEmails("Jon");
-        
-        //emails.deleteEmail("Jon", 0);
-    }
+    private Socket clientLink;
+    private EmailStore emailStore;
+    private UserStore userStore;
+    private Scanner input;
+    private PrintWriter output;
     
+    private String name;
+    private ThreadGroup group;
+            
+    
+    
+    //EmailService newClient = new EmailService(group, clientLink.getInetAddress(), clientLink, emailstore, userstore);
+
+   // public EmailService(ThreadGroup group, String name, Socket clientLink, EmailStore emailStore, UserStore userStore) {
+        public EmailService(Socket clientLink, EmailStore emailStore, UserStore userStore) {
+        //super(group, name);
+        
+        try{
+            
+            this.clientLink = clientLink;
+            this.emailStore = emailStore;
+            this.userStore = userStore;
+            
+            // Create the stream for reading from the Client
+            input = new Scanner(new InputStreamReader(this.clientLink.getInputStream()));
+            // Create the stream for writing to the Client
+            output = new PrintWriter(this.clientLink.getOutputStream(), true);
+        }catch (IOException e)
+        {
+            System.out.println("An exception occurred while setting up connection links for a thread: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        String message = "";
+            String response;
+        try {
+
+            //InetAddress response = clientLink.getInetAddress();
+            int responsePort = clientLink.getPort();
+
+            
+            
+
+            while (!message.equals("EXIT")) {
+                response = null;
+                System.out.println("Before nextline");
+
+                message = input.nextLine();
+                System.out.println("After nextline. user said: " + message);
+
+                //System.out.println("Message received: " + message + ", from " + response + " on port " + responsePort);
+
+                String[] components = message.split("%%");
+                switch (components[0]) {
+                    case "LOGIN":
+
+                        break;
+                    case "REGISTER":
+                        response = "ECHO%%Got your message, it was" + components[1];
+                        
+                        break;
+                    case "LOGOUT":
+
+                        break;
+                    case "SEND_MAIL":
+
+                        break;
+                    case "GET_UNREAD_EMAILS":
+
+                        break;
+                    case "GET_READ_EMAILS":
+
+                        break;
+                    case "GET_SPAM":
+
+                        break;
+                    case "SEARCH_EMAILS":
+
+                        break;
+                    case "DELETE_EMAILS":
+
+                        break;
+                    case "MARK_SPAM":
+
+                        break;
+                    case "DELETE_SPAM":
+
+                        break;
+                }
+                output.println(response);
+                output.flush();
+            }
+            clientLink.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(EmailServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+//        Command command = new LoginCommand();
+//        System.out.println(command.execute());
+//        
+//        UserStore users = new UserStore();
+//        users.login("Test", "123456");
+//        //users.test();
+//        
+//        
+//        String test = "LOGIN%%";
+//        
+//        EmailStore emails = new EmailStore();
+//        emails.sendEmail("Bob", "1/11/1111", "FirstEmail", "Hi, this is an email", "Jon");
+//        emails.sendEmail("Bob", "1/11/1111", "SecEmail", "qwetretrt", "Jon");
+//
+//        emails.sendEmail("Jon", "1/11/1111", "123Email", "TestLorem", "Bob");
+//        //emails.test();
+//        
+//        //System.out.println(emails.getAllUnreadEmails("Jon").toString());
+//        
+//        System.out.println(emails.getAllUnreadEmails("Jon").toString());
+//        System.out.println(emails.getAllUnreadEmails("Jon").toString());
+//        //emails.getAllUnreadEmails("Jon");
+//        
+//        //emails.deleteEmail("Jon", 0);
 }
